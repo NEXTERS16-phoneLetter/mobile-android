2e5865fe42f3de391e9fc3c471b50df34476e40d
KIYEON KIM <giyeon15@nate.com>
2020-02-18 20:40:28 +0900

    back Presse Button Custom Bottom Nav ViewPager2 Connect with Tab

--- app/src/main/java/com/nexters/towhom/ui/home/HomeFragment.kt
+++ app/src/main/java/com/nexters/towhom/ui/home/HomeFragment.kt
@@ -6,16 +6,23 @@ import android.util.Log
 import android.view.MotionEvent
 import android.view.View
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.widget.AppCompatImageButton
 import androidx.appcompat.widget.AppCompatImageView
 import androidx.navigation.fragment.findNavController
+import com.nexters.towhom.MainActivity
 import com.nexters.towhom.R
+import com.nexters.towhom.core.BindingActivity
 import com.nexters.towhom.core.BindingFragment
 import com.nexters.towhom.databinding.FragmentHomeBinding
 import org.koin.androidx.viewmodel.ext.android.getViewModel
 
-class HomeFragment : BindingFragment<FragmentHomeBinding>() {
+class HomeFragment : BindingFragment<FragmentHomeBinding>(), BindingActivity.OnBackPressedListener {
     override fun getLayoutResId() = R.layout.fragment_home
+
+    private var BACKPRESSED_TIME: Long = 0
+    private val FINISH_INTERVAL_TIME: Int = 2000
+
 
     private lateinit var letterImage : AppCompatImageView
 
@@ -27,6 +34,7 @@ class HomeFragment : BindingFragment<FragmentHomeBinding>() {
         letterImage = binding.letterStartImage
 
 
+        (activity as MainActivity).setOnKeyBackPressedListener(this) //backPressed 처리
         //binding.userName.text = "From. " + CommonData.USER_NAME
         //GlideBindingAdapter.setProfileImage(binding.userImage, CommonData.USER_IMAGE_URL)
 
@@ -61,6 +69,18 @@ class HomeFragment : BindingFragment<FragmentHomeBinding>() {
     }
 
     override fun bindingObserver() {
+    }
+
+    override fun onBackPressed() {
+        val tempTime = System.currentTimeMillis()
+        val intervalTime = tempTime - BACKPRESSED_TIME
+
+        if (intervalTime in 0..FINISH_INTERVAL_TIME) {
+            activity!!.finish()
+        } else {
+            BACKPRESSED_TIME = tempTime
+            Toast.makeText(context, resources.getString(R.string.sys_backpress_msg), Toast.LENGTH_SHORT).show()
+        }
     }
 
 
