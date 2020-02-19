9526afcc23172c3109109012381a5f0d14957cf5
hyunss <khh3599@naver.com>
2020-02-20 01:15:19 +0900

    gallery add

--- app/src/main/java/com/nexters/towhom/ui/write/ContentAdapter.kt
+++ app/src/main/java/com/nexters/towhom/ui/write/ContentAdapter.kt
@@ -1,18 +1,16 @@
 package com.nexters.towhom.ui.write
 
-import android.R.attr
 import android.app.Activity.RESULT_OK
 import android.content.Intent
 import android.net.Uri
-import android.provider.MediaStore
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.EditText
 import android.widget.ImageView
-import android.widget.MediaController
 import android.widget.Toast
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
@@ -25,8 +23,7 @@ class ContentAdapter(private var items: MutableList<String>) :
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContentViewHolder {
-        val view = LayoutInflater.from(parent.context)
-            .inflate(R.layout.fragment_write_content, parent, false)
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.fragment_write_content, parent, false)
         return ContentViewHolder(view)
 
 
@@ -35,7 +32,7 @@ class ContentAdapter(private var items: MutableList<String>) :
     override fun getItemCount(): Int = items.size
 
     override fun onBindViewHolder(holder: ContentViewHolder, position: Int) {
-        holder.bind(position)
+
     }
 
     fun setUpdateItems(items: MutableList<String>) {
@@ -43,33 +40,27 @@ class ContentAdapter(private var items: MutableList<String>) :
     }
 
 
+
     inner class ContentViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         private lateinit var letterEdit: EditText
-        private lateinit var galleryContainer: ConstraintLayout
-        private val GALLERY = 0
-
+        private lateinit var galleryImage : ConstraintLayout
+        private val GALLERY = 10
         init {
-            letterEdit = itemView.findViewById<EditText>(R.id.letter_text)
-            galleryContainer = itemView.findViewById(R.id.gallery_image)
-
+            letterEdit=itemView.findViewById<EditText>( R.id.letter_text)
+            galleryImage = itemView.findViewById(R.id.gallery_image)
 
             /** edittext 줄 수 제한 **/
-            letterEdit.addTextChangedListener(object : TextWatcher {
+            letterEdit.addTextChangedListener(object:TextWatcher{
                 var previousstirng = ""
                 override fun afterTextChanged(s: Editable?) {
-                    if (letterEdit.lineCount >= 11) {
+                    if(letterEdit.lineCount >= 11){
                         Toast.makeText(itemView.context, "편지지를 추가해주세요", Toast.LENGTH_SHORT).show()
                         letterEdit.setText(previousstirng)
                         letterEdit.setSelection(letterEdit.length())
                     }
                 }
 
-                override fun beforeTextChanged(
-                    s: CharSequence?,
-                    start: Int,
-                    count: Int,
-                    after: Int
-                ) {
+                override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
                     previousstirng = s.toString()
                 }
 
@@ -78,29 +69,31 @@ class ContentAdapter(private var items: MutableList<String>) :
 
             })
 
-            galleryContainer.setOnClickListener {
-                val intent: Intent = Intent(Intent.ACTION_GET_CONTENT)
-                intent.type = "image/*"
+            galleryImage.setOnClickListener{
+                val intent = Intent(Intent.ACTION_PICK)
+                intent.type="image/*"
+                Log.d("asdf","ok1")
                 (itemView.context as MainActivity).startActivityForResult(intent, GALLERY)
-
+                Log.d("asdf", GALLERY.toString())
             }
+/**
+             override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+                if(requestCode == GALLERY && resultCode == RESULT_OK ){
+                    val selectedImageUri: Uri ? = data?.data
+                    Log.d("asdf","ok3")
+                    galleryImage.setImageURI(selectedImageUri)
+
+                }
+            }
+             **/
 
         }
 
-        fun bind(position: Int) {
 
-        }
-        /**
-        fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        if(requestCode == GALLERY && resultCode == RESULT_OK ){
-        val selectedImageUri: Uri ? = data?.data
-        =
-        galleryImage.setImageURI(selectedImageUri)
-        }
-        }
 
-         **/
+
 
     }
+
 
 }
